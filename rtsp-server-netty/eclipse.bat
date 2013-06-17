66dd8cf894f236abad2302bad0ff215a3bd94776
darkmi <darkmi@126.com>
2013-06-17 17:02:00 +0800

    修改忽略文件列表.

--- rtsp-server-netty/eclipse.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo [INFO] Use maven eclipse plugin download jar and generate eclipse project files.
-echo [INFO] Please add "-Declipse.workspace=<path-to-eclipse-workspace>" at end of follow command.
-
-cd %~dp0
-call mvn eclipse:clean eclipse:eclipse
-
-pause
--- rtsp-server-netty/install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-cd %~dp0
-call mvn clean install -Dmaven.test.skip=true
-
-pause
