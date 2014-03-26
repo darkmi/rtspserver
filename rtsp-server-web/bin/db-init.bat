f9f2ccabf066476e87cefd93fc219ff468a42fa8
darkmi <darkmi@126.com>
2014-03-26 14:50:33 +0800

    项目重构.

--- rtsp-server-web/bin/db-init.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo [INFO] Create schema by sql, and import default data from src/main/resources/data/default-data.xml by dbunit.
-
-cd %~dp0
-call ant init-db
-pause
--- rtsp-server-web/src/main/webapp/common/footer.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page language="java" pageEncoding="UTF-8" %>
-<div id="ft">
-	Copyright &copy; 2005-2009 <a href="http://springside.org.cn">springside.org.cn</a>
-</div>
-
